3c34c7b5170df721c3e5ac7e79fac46fc5716222
Ryan Hasan Sunny <132351176+RyanHasanSunny@users.noreply.github.com>
2024-10-19 02:54:20 +0600

    portfolio updated scrolling effect

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -4,7 +4,9 @@ import '../Styles/PortfolioItem.css';
 const PortfolioItem = ({ image, title, link }) => {
   return (
     <div className="portfolio-item">
+      <div className="Imageplaceholder">
       <img src={image} alt={title} className="portfolio-image" />
+      </div>
       <div className="portfolio-content">
         <h3>{title}</h3>
       </div>
