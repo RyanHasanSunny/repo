417783ffba8c827087388918a54d1d3363899099
Ryan Hasan Sunny <ryanhasansunny31@gmail.com>
2025-02-21 19:48:09 +0600

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -45,7 +45,7 @@ const Contact = () => {
 
     // Send form data to the backend
     try {
-      const response = await fetch('/.netlify/functions/sendEmail', {
+      const response = await fetch('http://localhost:5000/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
